6388d676324c644ad28155c774ba5998cf793456
davidhcsic <davidh@iim.csic.es>
2020-04-23 16:33:01 +0200

    Create exponenciais_1.py

---
+++ exponenciais/exponenciais_1.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 23 16:19:27 2020
+
+@author: David
+"""
+
+
+
+import matplotlib.pyplot as plt
+import numpy as np
+from numpy import array
+
+# 100 linearly spaced numbers
+x = np.linspace(-5,5,100)
+
+# Define a row vector
+v = array([-5, -4, -3 , -2,-1, 0, 1, 2, 3 ,4 ,5])
+
+# the function, which is y = x^2 here
+
+# setting the axes at the centre
+fig = plt.figure()
+
+# plot the function
+plt.plot(x,pow(x,1.3), 'r')
+
+# show the plot
+plt.show()
+
